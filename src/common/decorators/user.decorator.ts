2c5e0e7c8be61447c638fdc4e3a73b663ff8443f
Vladimir Basko <1989bvg@gmail.com>
2023-02-15 15:55:37 +0100

    small error fixing

--- src/common/decorators/user.decorator.ts
+++ src/common/decorators/user.decorator.ts
@@ -3,7 +3,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const User = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    const user = request.context.user;
+    const user = request?.context?.user;
 
     return data ? user?.[data] : user;
   },
