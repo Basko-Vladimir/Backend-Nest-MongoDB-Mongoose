d2392383511bd55d3072279363b7a7fdd98f29db
Vladimir Basko <1989bvg@gmail.com>
2023-03-22 15:52:24 +0300

    fixed e2e comments tests without tests for 403 exceptions

--- test/comments.e2e-spec.ts
+++ test/comments.e2e-spec.ts
@@ -9,28 +9,37 @@ import {
   likes,
 } from './mockData';
 import { INestApplication } from '@nestjs/common';
+import { publicCommentsRequests } from './utils/comments-requests';
+import { adminUsersRequests } from './utils/users-requests';
+import { publicPostsRequests } from './utils/posts-requests';
 import {
-  createBlogsRequest,
-  createCommentByPostIdRequest,
-  createPostRequest,
-  createUserRequest,
-  deleteCommentRequest,
-  getCommentRequest,
-  initTestApp,
-  loginRequest,
-  updateCommentLikeStatus,
-  updateCommentRequest,
-  updatePostLikeStatus,
-} from './utils/utils';
+  adminBlogsRequests,
+  bloggerBlogsRequests,
+} from './utils/blogs-requests';
+import { authRequests } from './utils/auth-requests';
 import { LikeStatus } from '../src/common/enums';
 import { IFullCommentOutputModel } from '../src/comments/api/dto/comments-output-models.dto';
+import { initTestApp } from './utils/common';
 
 describe('Comments', () => {
-  jest.setTimeout(60 * 1000);
+  jest.setTimeout(20 * 1000);
   const { correctCreateBlogDtos } = blogs;
-  const { notFoundException } = errors;
+  const { notFoundExceptionMock } = errors;
   const { correctCreatePostDtos, correctUpdatePostDto } = posts;
-  const { correctBasicCredentials, incorrectAccessToken } = auth;
+  const { correctBasicCredentials, incorrectAccessToken, getBearerAuthHeader } =
+    auth;
+  const { createUserRequest } = adminUsersRequests;
+  const { createBlogsRequest, createPostByBlogIdRequest } =
+    bloggerBlogsRequests;
+  const {
+    getCommentRequest,
+    deleteCommentRequest,
+    updateCommentRequest,
+    updateCommentLikeStatus,
+  } = publicCommentsRequests;
+  const { bindBlogWithUser } = adminBlogsRequests;
+  const { loginRequest } = authRequests;
+  const { createCommentByPostIdRequest } = publicPostsRequests;
   const {
     correctCreateCommentDtos,
     commentsBadQueryResponse,
@@ -45,7 +54,8 @@ describe('Comments', () => {
   } = likes;
   const { correctCreateUserDtos } = users;
   let app: INestApplication;
-  let post1;
+  let post1, post2;
+  let user1, user2, user3;
   let comment1;
   let blog1;
   let user1Token;
@@ -60,15 +70,16 @@ describe('Comments', () => {
         .set(correctBasicCredentials)
         .send(correctCreateUserDtos[0]);
       expect(response1.status).toBe(201);
+      user1 = response1.body;
     });
 
     it('User login', async () => {
-      const response = await loginRequest(app).send({
+      const response1 = await loginRequest(app).send({
         loginOrEmail: correctCreateUserDtos[0].login,
         password: correctCreateUserDtos[0].password,
       });
-      expect(response.status).toBe(200);
-      user1Token = `Bearer ${response.body.accessToken}`;
+      expect(response1.status).toBe(200);
+      user1Token = `Bearer ${response1.body.accessToken}`;
     });
 
     it('Blog creating', async () => {
@@ -79,17 +90,24 @@ describe('Comments', () => {
       blog1 = response.body;
     });
 
-    it('Post creating', async () => {
-      const response = await createPostRequest(app)
-        .set(correctBasicCredentials)
-        .send({ ...correctCreatePostDtos[0], blogId: blog1.id });
-      expect(response.status).toBe(201);
-      post1 = response.body;
+    it('binding of blog with user', async () => {
+      const response = await bindBlogWithUser(app, blog1.id, user1.id).set(
+        correctBasicCredentials,
+      );
+      expect(response.status).toBe(204);
+    });
+
+    it('Posts creating', async () => {
+      const response1 = await createPostByBlogIdRequest(app, blog1.id)
+        .set(getBearerAuthHeader(user1Token))
+        .send(correctCreatePostDtos[0]);
+      expect(response1.status).toBe(201);
+      post1 = response1.body;
     });
 
     it('Comment creating', async () => {
       const response = await createCommentByPostIdRequest(app, post1.id)
-        .set({ Authorization: user1Token })
+        .set(getBearerAuthHeader(user1Token))
         .send(correctCreateCommentDtos[0]);
       expect(response.status).toBe(201);
       comment1 = response.body;
@@ -98,15 +116,15 @@ describe('Comments', () => {
 
   describe('/(GET ONE COMMENT) get one comment', () => {
     it('by invalid id', async () => {
-      const response3 = await getCommentRequest(app, INVALID_ID);
-      expect(response3.status).toBe(404);
+      const response = await getCommentRequest(app, INVALID_ID);
+      expect(response.status).toBe(404);
     });
 
     it('by valid id', async () => {
-      const response3 = await getCommentRequest(app, comment1.id);
-      expect(response3.status).toBe(200);
-      expect(response3.body).toEqual(
-        getCommentItem(comment1.content, correctCreateUserDtos[0].login),
+      const response = await getCommentRequest(app, comment1.id);
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(
+        getCommentItem(comment1.content, user1.login),
       );
     });
   });
@@ -114,37 +132,44 @@ describe('Comments', () => {
   describe('/(UPDATE ONE COMMENT)', () => {
     it('incorrect token or without it', async () => {
       const response1 = await updateCommentRequest(app, post1.id).send(
-        correctUpdatePostDto,
+        correctUpdateCommentDto,
       );
       expect(response1.status).toBe(401);
 
       const response2 = await updateCommentRequest(app, post1.id)
-        .set({ Authorization: incorrectAccessToken })
+        .set(getBearerAuthHeader(incorrectAccessToken))
         .send(correctUpdateCommentDto);
       expect(response2.status).toBe(401);
     });
 
     it('correct token but invalid id', async () => {
       const response = await updateCommentRequest(app, INVALID_ID)
-        .set({ Authorization: user1Token })
+        .set(getBearerAuthHeader(user1Token))
         .send(correctUpdateCommentDto);
       expect(response.status).toBe(404);
-      expect(response.body).toEqual(notFoundException);
+      expect(response.body).toEqual(notFoundExceptionMock);
     });
 
     it('correct token, valid id but incorrect input data', async () => {
       for (let i = 0; i < incorrectCommentsDtos.length; i++) {
         const response = await updateCommentRequest(app, comment1.id)
-          .set({ Authorization: user1Token })
+          .set(getBearerAuthHeader(user1Token))
           .send(incorrectCommentsDtos[i]);
         expect(response.status).toBe(400);
         expect(response.body).toEqual(commentsBadQueryResponse);
       }
     });
 
+    // it('correct all (auth credentials, id, input data) but blog is someone else', async () => {
+    //   const response = await updateBlogRequest(app, blog4.id)
+    //     .set(getBearerAuthHeader(user1Token))
+    //     .send(correctUpdateBlogDto);
+    //   expect(response.status).toBe(403);
+    // });
+
     it('correct all (token, id, input data)', async () => {
       const response1 = await updateCommentRequest(app, comment1.id)
-        .set({ Authorization: user1Token })
+        .set(getBearerAuthHeader(user1Token))
         .send(correctCreateCommentDtos[0]);
       expect(response1.status).toBe(204);
 
@@ -162,24 +187,24 @@ describe('Comments', () => {
       );
       expect(response1.status).toBe(401);
 
-      const response2 = await updatePostLikeStatus(app, comment1.id)
-        .set({ Authorization: incorrectAccessToken })
+      const response2 = await updateCommentLikeStatus(app, comment1.id)
+        .set(getBearerAuthHeader(incorrectAccessToken))
         .send(correctUpdateLikeStatusDto[0]);
       expect(response2.status).toBe(401);
     });
 
     it('correct token but invalid id', async () => {
       const response = await updateCommentLikeStatus(app, INVALID_ID)
-        .set({ Authorization: user1Token })
+        .set(getBearerAuthHeader(user1Token))
         .send(correctUpdateLikeStatusDto[0]);
       expect(response.status).toBe(404);
-      expect(response.body).toEqual(notFoundException);
+      expect(response.body).toEqual(notFoundExceptionMock);
     });
 
     it('correct token, valid id but incorrect input data', async () => {
       for (let i = 0; i < incorrectLikeStatusDto.length; i++) {
         const response = await updateCommentLikeStatus(app, comment1.id)
-          .set({ Authorization: user1Token })
+          .set(getBearerAuthHeader(user1Token))
           .send(incorrectLikeStatusDto[i]);
         expect(response.status).toBe(400);
         expect(response.body).toEqual(likeBadQueryResponse);
@@ -194,7 +219,7 @@ describe('Comments', () => {
           correctUpdateLikeStatusDto[i].likeStatus === LikeStatus.DISLIKE;
 
         const response1 = await updateCommentLikeStatus(app, comment1.id)
-          .set({ Authorization: user1Token })
+          .set(getBearerAuthHeader(user1Token))
           .send(correctUpdateLikeStatusDto[i]);
         expect(response1.status).toBe(204);
 
@@ -219,29 +244,36 @@ describe('Comments', () => {
       const response1 = await deleteCommentRequest(app, comment1.id);
       expect(response1.status).toBe(401);
 
-      const response2 = await deleteCommentRequest(app, comment1.id).set({
-        Authorization: incorrectAccessToken,
-      });
+      const response2 = await deleteCommentRequest(app, comment1.id).set(
+        getBearerAuthHeader(incorrectAccessToken),
+      );
       expect(response2.status).toBe(401);
     });
 
     it('correct token but invalid id', async () => {
-      const response = await deleteCommentRequest(app, INVALID_ID).set({
-        Authorization: user1Token,
-      });
+      const response = await deleteCommentRequest(app, INVALID_ID).set(
+        getBearerAuthHeader(user1Token),
+      );
       expect(response.status).toBe(404);
-      expect(response.body).toEqual(notFoundException);
+      expect(response.body).toEqual(notFoundExceptionMock);
     });
 
+    // it('correct all (auth credentials, id, input data) but blog is someone else', async () => {
+    //   const response = await updateBlogRequest(app, blog4.id)
+    //     .set(getBearerAuthHeader(user1Token))
+    //     .send(correctUpdateBlogDto);
+    //   expect(response.status).toBe(403);
+    // });
+
     it('correct token and valid id', async () => {
-      const response = await deleteCommentRequest(app, comment1.id).set({
-        Authorization: user1Token,
-      });
+      const response = await deleteCommentRequest(app, comment1.id).set(
+        getBearerAuthHeader(user1Token),
+      );
       expect(response.status).toBe(204);
 
       const response2 = await getCommentRequest(app, comment1.id);
       expect(response2.status).toBe(404);
-      expect(response2.body).toEqual(notFoundException);
+      expect(response2.body).toEqual(notFoundExceptionMock);
     });
   });
 
