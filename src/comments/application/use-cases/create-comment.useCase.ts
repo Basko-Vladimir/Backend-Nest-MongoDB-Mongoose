8709d87b782401761b49ecef408308f2c4b1bd23
Vladimir Basko <1989bvg@gmail.com>
2023-03-29 16:34:55 +0300

    adjusted logic of CreateCommentUseCase

--- src/comments/application/use-cases/create-comment.useCase.ts
+++ src/comments/application/use-cases/create-comment.useCase.ts
@@ -1,10 +1,11 @@
-import { NotFoundException } from '@nestjs/common';
+import { ForbiddenException, NotFoundException } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { InjectModel } from '@nestjs/mongoose';
 import { Comment, CommentModelType } from '../../schemas/comment.schema';
 import { CreateCommentDto } from '../../api/dto/create-comment.dto';
 import { CommentsRepository } from '../../infrastructure/comments.repository';
 import { PostsRepository } from '../../../posts/infrastructure/posts.repository';
+import { UsersRepository } from '../../../users/infrastructure/users.repository';
 
 export class CreateCommentCommand {
   constructor(public createCommentDto: CreateCommentDto) {}
@@ -17,6 +18,7 @@ export class CreateCommentUseCase
   constructor(
     private commentsRepository: CommentsRepository,
     private postsRepository: PostsRepository,
+    private usersRepository: UsersRepository,
     @InjectModel(Comment.name) protected CommentModel: CommentModelType,
   ) {}
 
@@ -25,8 +27,18 @@ export class CreateCommentUseCase
     const targetPost = await this.postsRepository.findPostById(
       createCommentDto.postId,
     );
-
+    const targetUser = await this.usersRepository.findUserById(
+      createCommentDto.userId,
+    );
     if (!targetPost) throw new NotFoundException();
+
+    const isBannedUserForCurrenBlog = Boolean(
+      targetUser.bannedForBlogs.find(
+        (item) => item.blogId === String(targetPost.blogId),
+      ),
+    );
+
+    if (isBannedUserForCurrenBlog) throw new ForbiddenException();
 
     const createdComment = this.CommentModel.createCommentEntity(
       createCommentDto,
